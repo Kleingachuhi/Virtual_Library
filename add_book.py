5fccfc513773d5faad0e49b35f1edece23892dd4
Klein <klein.gachuhi@student.moringaschool.com>
2025-05-29 20:00:05 +0300

    Adding books completed

---
+++ add_book.py
@@ -0,0 +1,23 @@
+from database import SessionLocal
+from models.book import Book
+from models.author import Author
+
+def add_book(title, author_id):
+    session = SessionLocal()
+
+    author = session.query(Author).filter_by(id=author_id).first()
+    if not author:
+        print(f"Author with ID {author_id} does not exist.")
+        session.close()
+        return
+
+    new_book = Book(title=title, author_id=author_id)
+    session.add(new_book)
+    session.commit()
+    print(f"New Book: '{new_book.title}' added for Author ID {author_id}")
+
+    session.close()
+
+if __name__ == "__main__":
+    add_book("The Hobbit", 1)
+    add_book("Chilling adventures of Klein", 2)
